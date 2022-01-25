96008206cd3b72e35f361633a287a8a4043eac08
jlmitche23 <jacquelinemitchell2019@gmail.com>
2022-01-24 21:41:57 -0800

    Adding Working Model + DataLoading

---
+++ ECS189G_Winter_2022_Source_Code_Template/code/stage_2_code/Dataset_Loader.py
@@ -0,0 +1,28 @@
+from code.base_class.dataset import dataset
+import pandas as pd
+import numpy as np
+
+class Dataset_Loader(dataset):
+    data = None
+    dataset_source_folder_path = None
+    dataset_source_file_name = None
+
+    def __init__(self, dName=None, dDescription=None):
+        super().__init__(dName, dDescription)
+
+    def load(self):
+        print('Loading data...!!!!')
+
+        f = open(self.dataset_source_folder_path + "train.csv", 'r')
+        data = np.loadtxt(f, delimiter=",")
+        X_train = data[:, 1:]
+        y_train = data[:, 0]
+        f.close()
+
+        f = open(self.dataset_source_folder_path + "test.csv", 'r')
+        data = np.loadtxt(f, delimiter=",")
+        X_test = data[:, 1:]
+        y_test = data[:, 0]
+        f.close()
+
+        return {'X_train': X_train, 'y_train': y_train, 'X_test' : X_test, 'y_test' : y_test}
--- ECS189G_Winter_2022_Source_Code_Template/code/stage_2_code/Method_MLP.py
+++ ECS189G_Winter_2022_Source_Code_Template/code/stage_2_code/Method_MLP.py
@@ -1,24 +1,28 @@
 import torch
 import torch.nn as nn
+from code.base_class.method import method
+import numpy as np
+from code.stage_2_code.Evaluate_Accuracy import Evaluate_Accuracy
 
-class MLP(nn.Module):
+class Method_MLP(method, nn.Module):
 
     # Class variables
     data = None
     iterations = 3000
     batch_size = 100
-    learning_rate = 1e-1
+    learning_rate = 1e-3
 
-    def __init__(self):
+    def __init__(self, mName, mDescription):
         """Initialization"""
-        super(MLP, self).__init__()
+        method.__init__(self, mName, mDescription)
+        nn.Module.__init__(self)
 
         self.fc_1 = nn.Linear(784, 100)
         self.act_1 = nn.ReLU()
         self.fc_2 = nn.Linear(100, 100)
         self.act_2 = nn.ReLU()
         self.fc_3 = nn.Linear(100, 10) # 10 Output classes in MNIST Dataset
-        self.act_prob = nn.Softmax(dim=1)
+        #self.act_prob = nn.Softmax(dim=1)
 
     def forward(self, input_x):
         """Propagate Forward
@@ -26,14 +30,14 @@ class MLP(nn.Module):
         :param input_x: arguments to be put through a forward pass
 
         """
-        input_x = self.fc_1(input_x)
-        input_x = self.act_1(input_x)
-        input_x = self.fc_2(input_x)
-        input_x = self.act_2(input_x)
-        input_x = self.fc_3(input_x)
-        output_predictions = self.act_prob(input_x)
+        out = self.fc_1(input_x)
+        out = self.act_1(out)
+        out = self.fc_2(out)
+        out = self.act_2(out)
+        out = self.fc_3(out)
+        #output_predictions = self.act_prob(input_x)
 
-        return output_predictions
+        return out
 
     def train(self, X, y):
         """
@@ -46,35 +50,48 @@ class MLP(nn.Module):
 
         # Optimizer and Loss function
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        loss_criterion = torch.nn.NLLLoss() # useful for categorical classification tasks (just arbitrary for now)
+        loss_criterion = torch.nn.CrossEntropyLoss()
+        accuracy_evaluator = Evaluate_Accuracy('training evaluator', '')
 
-        # Set up data loading process
-        train_loader = None # needs to be set up
-        test_loader = None
+        X = torch.FloatTensor(X)
+        y = torch.LongTensor(y)
 
-        for epoch in range(n_epochs):
+        print(f"Running for {n_epochs} epochs...")
+
+        iter = 0
+
+        for epoch in range(int(n_epochs)):
             # This is set up so that we can run mini-batches one at time, although
             # this assumes that the train data was set up to be enumerated in the correct way.
-            for i, (train_d, labels) in enumerate(train_loader):
 
-                # May need to change depending on how the data is loaded
-                # I assume for now that train_d passes us tensors.
-                train_d.requires_grad()
+            # Mini-batching code credit: https://stackoverflow.com/questions/45113245/how-to-get
+            # -mini-batches-in-pytorch-in-a-clean-and-efficient-way
+            permutation = torch.randperm(X.size()[0])
 
+            for i in range(0, X.size()[0], self.batch_size):
                 # Do this for each batch, as PyTorch accumulates gradients
                 optimizer.zero_grad()
 
+                indices = permutation[i:i + self.batch_size]
+                batch_x, batch_y = X[indices], y[indices]
+
                 # Forward pass
-                outputs_pred = self.forward(train_d) # again, may need to edit how train_d is passed depending on dtype
-
-                loss = loss_criterion(outputs_pred, labels)
-
+                outputs_pred = self.forward(batch_x) # again, may need to edit how train_d is passed depending on dtype
+                loss = loss_criterion(outputs_pred, batch_y)
+                # Update params
                 loss.backward() # back-prop the errors
                 optimizer.step() # Update via gradient descent
 
-                if epoch % 50 == 0:
+                iter += 1
+
+                if iter % 500 == 0:
                     # evaluate how training process is going... WIP.
                     print("Current Progress::....")
+                    #print(batch_y.shape)
+                    current_full_outputs = self.forward(X)
+                    accuracy_evaluator.data = {'true_y': y, 'pred_y': current_full_outputs.max(1)[1]}
+                    print('Epoch:', epoch, 'Accuracy:', accuracy_evaluator.evaluate(), 'Loss:', loss.item())
+
 
     def test(self, X):
         """
@@ -87,13 +104,13 @@ class MLP(nn.Module):
     def run(self):
         """
         NOTE: JUST TEMPLATED.  Not meant to be functional yet.
-
         """
         print('Beginning Training Process...')
-        self.train(train_data, train_data_labels)
+        self.train(self.data['train']['X'], self.data['train']['y'])
         print('Test Results')
-        pred_y = self.test(test_data)
-        #return {'pred_y': pred_y, 'true_y': self.data['test']['y']}
+        pred_y = self.test(self.data['test']['X'])
+
+        return {'pred_y': pred_y, 'true_y': self.data['test']['y']}
 
 
 
