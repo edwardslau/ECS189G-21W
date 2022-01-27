9705bbe8696ca0c9c80925ec893eee8de5c3c46c
tejessri <tejes.srivastava@gmail.com>
2022-01-26 17:06:16 -0800

    Added different metrics to the evaluate class

--- ECS189G_Winter_2022_Source_Code_Template/code/stage_2_code/Evaluate_Accuracy.py
+++ ECS189G_Winter_2022_Source_Code_Template/code/stage_2_code/Evaluate_Accuracy.py
@@ -6,7 +6,7 @@ Concrete Evaluate class for a specific evaluation metrics
 # License: TBD
 
 from code.base_class.evaluate import evaluate
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 class Evaluate_Accuracy(evaluate):
@@ -14,4 +14,11 @@ class Evaluate_Accuracy(evaluate):
 
     def evaluate(self):
         print('evaluating performance...')
+
+        if self.evaluate_name == 'precision':
+            return precision_score(self.data['true_y'], self.data['pred_y'])
+        if self.evaluate_name == 'recall':
+            return recall_score(self.data['true_y'], self.data['pred_y'])
+        if self.evaluate_name == 'f1':
+            return f1_score(self.data['true_y'], self.data['pred_y'])
         return accuracy_score(self.data['true_y'], self.data['pred_y'])
