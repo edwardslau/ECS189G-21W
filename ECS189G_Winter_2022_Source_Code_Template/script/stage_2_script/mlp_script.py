a3c5ad0458a476d8b5c12f87f0df5beb6602e950
jlmitche23 <jacquelinemitchell2019@gmail.com>
2022-01-27 20:08:47 -0800

    Updated script

--- ECS189G_Winter_2022_Source_Code_Template/script/stage_2_script/mlp_script.py
+++ ECS189G_Winter_2022_Source_Code_Template/script/stage_2_script/mlp_script.py
@@ -39,9 +39,7 @@ if 1:
     print('************ Start ************')
     setting_obj.prepare(data_obj_train, method_obj, result_obj, evaluate_obj)
     setting_obj.print_setup_summary()
-    mean_score, std_score = setting_obj.load_run_save_evaluate()
-    print('************ Overall Performance ************')
-    print('MLP Accuracy: ' + str(mean_score) + ' +/- ' + str(std_score))
+    setting_obj.load_run_save_evaluate()
     print('************ Finish ************')
     # ------------------------------------------------------
 
