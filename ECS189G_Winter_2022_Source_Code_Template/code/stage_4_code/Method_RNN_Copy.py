8c2dafd05bb90e2bcaefd1c83e481ebadd3590f8
jlmitche23 <jacquelinemitchell2019@gmail.com>
2022-02-21 09:24:05 -0800

    Adding model file

--- ECS189G_Winter_2022_Source_Code_Template/code/stage_4_code/Method_RNN_Copy.py
+++ ECS189G_Winter_2022_Source_Code_Template/code/stage_4_code/Method_RNN_Copy.py
@@ -1,8 +1,9 @@
 from code.base_class.method import method
-from code.stage_4_code.DataLoader_Classification import Dataset_Loader
+from code.stage_4_code.DataLoader_Class_2 import Dataset_Loader
 import torch
 import torch.nn as nn
 import numpy as np
+import torchtext
 
 np.random.seed(2)
 torch.manual_seed(2)
@@ -10,7 +11,8 @@ torch.manual_seed(2)
 
 class Method_RNN_Classification(method, nn.Module):
     data = None
-    learning_rate = 5e-03
+    learning_rate = 1e-03
+    vocab_w_i = None
 
     def __init__(self, mName, mDescription, vocab_size):
         method.__init__(self, mName, mDescription)
@@ -19,21 +21,24 @@ class Method_RNN_Classification(method, nn.Module):
         self.embedding_dim = 100
         self.hidden_dim = 200
         self.output_dim = 1
-        self.sequence_length = 200
+        self.sequence_length = 500
+        self.vocab_size = vocab_size
+        self.num_layers = 2
 
         # output of embedding: [sequence_length, batch_size, embed_dim]
-        self.embedding = nn.Embedding(vocab_size, self.embedding_dim)
+        self.embedding = nn.Embedding(self.vocab_size, self.embedding_dim)
         # output of RNN:
         # output: [sequence_length, batch_size, hidden_dim]
         # h_n: [num_layers, batch_size, hidden_dim] (Contains final hidden state for each
         # element in a batch)
-        self.rnn = nn.LSTM(self.embedding_dim, self.hidden_dim, num_layers=1, batch_first=True)
-        self.dropout = nn.Dropout(0.3)
+        self.rnn = nn.LSTM(self.embedding_dim, self.hidden_dim, num_layers=self.num_layers, batch_first=True)
+        self.dropout = nn.Dropout(0.5)
         # it is hidden_dim * sequence length because (we have sequence-length
         # iterations of the model over time and hidden_dim number of outputs
         # for each of those iterations.)
         self.fc = nn.Linear(self.hidden_dim, self.output_dim)
 
+        #self.sigmoid = torch.nn.Sigmoid()
         self.word_dict = None
 
     def forward(self, text, hid):
@@ -54,10 +59,12 @@ class Method_RNN_Classification(method, nn.Module):
         # print("outputshape: ", output.size())
         batch_size = output.shape[0] # record batch size
 
+        output = output.contiguous().view(-1, self.hidden_dim)
+
         #output = output.contiguous().view(-1, self.hidden_dim)
         output = self.dropout(output) # just some dropout
         o = self.fc(output) # results in [batch_size, sent_len, 1]
-        #o = o.view(batch_size, -1)
+        o = o.view(batch_size, -1)
 
         # print("Hidden squeeze: ", hidden.squeeze(0).shape)
         # o = self.fc(hidden.squeeze(0))
@@ -68,6 +75,22 @@ class Method_RNN_Classification(method, nn.Module):
 
     def train(self, X, y, X_test, y_test, vocab):
 
+        # Before we begin training, we load our vectors!
+        words_not_gloved = 0
+        glove = torchtext.vocab.GloVe(name="6B", dim=100)
+        for word in list(vocab.keys()):
+            try:
+                self.embedding.weight.data[vocab[word]] = glove.vectors[glove.stoi[word]]
+            except:
+                self.embedding.weight.data[vocab[word]] = torch.zeros(self.embedding_dim)
+                words_not_gloved += 1
+
+        print("Number of words without an embedding vector: ", words_not_gloved, " out of ", len(vocab))
+
+        # make the padding and unknown indexes start at 0
+        self.embedding.weight.data[0] = torch.zeros(self.embedding_dim)
+        self.embedding.weight.data[1] = torch.zeros(self.embedding_dim)
+
         X = torch.LongTensor(X) # X.size = torch.Size is (200,500)
         y = torch.FloatTensor(y)
 
@@ -77,6 +100,8 @@ class Method_RNN_Classification(method, nn.Module):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
         loss_fn = torch.nn.BCEWithLogitsLoss()
         batch_size = 64#1000
+
+        losses_per_epoch = []
 
         epochs = 5
         for epoch in range(epochs):
@@ -91,8 +116,8 @@ class Method_RNN_Classification(method, nn.Module):
 
                 batch_x, batch_y = X[i:i + batch_size, :], y[i:i+batch_size]
 
-                h0 = torch.zeros((1, batch_x.size()[0], self.sequence_length))
-                c0 = torch.zeros((1, batch_x.size()[0], self.sequence_length))
+                h0 = torch.zeros((self.num_layers, batch_x.size()[0], self.hidden_dim))
+                c0 = torch.zeros((self.num_layers, batch_x.size()[0], self.hidden_dim))
 
                 # print(batch_x[:, 0])
                 # print([vocab[i] for i in batch_x[:, 0]])
@@ -110,7 +135,7 @@ class Method_RNN_Classification(method, nn.Module):
                 loss.backward()
 
                 # Addresses exploding gradients...
-                nn.utils.clip_grad_norm_(self.parameters(), 5)
+                #nn.utils.clip_grad_norm_(self.parameters(), 5)
                 optimizer.step()
 
             if True:
@@ -130,8 +155,8 @@ class Method_RNN_Classification(method, nn.Module):
                         test_batch_x, test_batch_y = X_test[j:j+batch_size,:], y_test[j:j+batch_size]
 
                         # (num_layers, batch_size, hidden_dimension)
-                        h0 = torch.zeros((1, test_batch_x.size()[0], self.hidden_dim))
-                        c0 = torch.zeros((1, test_batch_x.size()[0], self.hidden_dim))
+                        h0 = torch.zeros((self.num_layers, test_batch_x.size()[0], self.hidden_dim))
+                        c0 = torch.zeros((self.num_layers, test_batch_x.size()[0], self.hidden_dim))
 
                         pred_test, h_out = self.forward(test_batch_x, (h0, c0))
                         pred_test = pred_test.squeeze(-1)
@@ -146,12 +171,49 @@ class Method_RNN_Classification(method, nn.Module):
                         total_corr += sum(correct)
                         print('Epoch:', epoch, 'Batch:', j, '  Accuracy:', acc, 'Loss:', loss.item())
                     print("TOTAL EPOCH TEST ACCURACY: ", total_corr / X_test.size()[0])
+                losses_per_epoch.append(loss.item())
+
+        with open("/Users/jacquelinemitchell/Documents/IMDB_classification_losses.npy", "wb") as f:
+            np.save(f, np.array(losses_per_epoch))
+
+    def test(self, X):
+
+        if self.training == False:
+            self.training = True
+
+        X = torch.LongTensor(X)
+        batch_size = 64
+
+        predictions = np.array([])
+
+        with torch.no_grad():
+            for i in range(0, X.size()[0], batch_size):
+
+                batch_x = X[i:i + batch_size, :]
+
+                h0 = torch.zeros((1, batch_x.size()[0], self.hidden_dim))
+                c0 = torch.zeros((1, batch_x.size()[0], self.hidden_dim))
+
+                pred_test, h_out = self.forward(batch_x, (h0, c0))
+                pred_test = pred_test.squeeze(-1)
+                rounded_preds = np.array(torch.round(torch.sigmoid(pred_test)))
+
+                predictions = np.append(predictions, rounded_preds)
+
+        return predictions
+
+    def run(self):
+        self.train(self.data['train']['X'], self.data['train']['y'],
+                   self.data['test']['X'], self.data['test']['y'], self.vocab_w_i)
+
+        a = self.test(self.data['test']['X'])
+        return {'pred_y' : a, 'true_y' : self.data['test']['y']}
 
 
 if __name__ == "__main__":
     DL = Dataset_Loader()
     data_dict, vocab = DL.load()
     mod = Method_RNN_Classification(mName="HI", mDescription="NO",
-                                    vocab_size=len(vocab) + 2)  # Note: the reason why we add
+                                    vocab_size=1001)#len(vocab) + 2)  # Note: the reason why we add
     # + 2 here is because we cannot forget about our dummy variables (0 : pad, 1 : unk)
     mod.train(data_dict["X_train"], data_dict["y_train"], data_dict["X_test"], data_dict["y_test"], vocab)
